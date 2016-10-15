8f5f8aebce97d5945cfa1db28b4aa575db757e23
Jeremy Wells <jemmyw@gmail.com>
2016-10-15 22:58:08 +1300

    Implement fulfillers

---
+++ functions/fulfillers/index.ts
@@ -0,0 +1,24 @@
+import * as apex from 'apex.js';
+import {RemoveTransform} from "../../lib/CommandToDataTransform";
+import {spread} from "../../lib/spread";
+import {StreamTransform} from "../../lib/StreamTransform";
+import {FulfillersCreateCommand} from 'sparks-schemas/types/commands/FulfillersCreate';
+
+const create = StreamTransform('Fulfillers.create', async function({domain, action, uid, payload: {values}}:FulfillersCreateCommand) {
+
+  return [{
+    streamName: 'data.firebase',
+    partitionKey: uid,
+    data: {
+      domain,
+      action,
+      key: [values.oppKey, values.teamKey].join('-'),
+      values
+    }
+  }];
+});
+
+export default apex(spread(
+  create,
+  RemoveTransform('Fulfillers.remove')
+));

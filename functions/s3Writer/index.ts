c239d93075dcf3daa7b79fad5cd1e6128786ce81
Jeremy Wells <jemmyw@gmail.com>
2016-10-27 09:33:43 +1300

    Add a service to write kinesis stream events to S3

---
+++ functions/s3Writer/index.ts
@@ -0,0 +1,53 @@
+import * as zlib from 'zlib';
+import * as apex from 'apex.js';
+import {S3} from 'aws-sdk';
+import {groupBy, head, last} from 'ramda'
+
+const bucket = process.env['BUCKET'];
+const s3Client:S3 = new S3({
+  params: {
+    Bucket: bucket
+  }
+});
+
+const byPartitionKey = groupBy<Lambda.KinesisEventRecord>(record =>
+  [
+    record.eventSourceARN.split('/').slice(-1)[0],
+    record.kinesis.partitionKey
+  ].join('/')
+);
+
+function gzip(data):Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    zlib.gzip(data, (err, buffer) => {
+      if (err) { return reject(err); }
+      resolve(buffer);
+    });
+  })
+}
+
+function savePartition(streamName:string, partitionKey:string, records:Lambda.KinesisEventRecord[]) {
+  const sequenceNumbers = records.map(r => r.kinesis.sequenceNumber);
+  const [from, to] = [head(sequenceNumbers), last(sequenceNumbers)]
+  const key = [streamName, partitionKey, [from, to].join('-')].join('/');
+  const data = records.map(record => record.kinesis.data.trim()).join("\n");
+
+  return gzip(data)
+    .then(buffer => s3Client.putObject({
+        Key: key,
+        Body: buffer,
+        ContentEncoding: 'gzip'
+      }).promise()
+    );
+}
+
+export default apex(async function(event:Lambda.KinesisEvent) {
+  const partitions = byPartitionKey(event.Records);
+  const promises = Object.keys(partitions).map(key => {
+    const [streamName, partitionKey] = key.split('/');
+    const records = partitions[key];
+    return savePartition(streamName, partitionKey, records);
+  });
+  return Promise.all(promises);
+});
+

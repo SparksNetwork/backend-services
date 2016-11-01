45ae2519f92e21e3c1789d6962fadc6863dd577e
Jeremy Wells <jemmyw@gmail.com>
2016-11-01 15:51:23 +1300

    chore(Build): refactor ecs deploy script

--- bin/ecs_deploy.ts
+++ bin/ecs_deploy.ts
@@ -2,14 +2,8 @@ require('source-map-support').install();
 
 import {ECS} from 'aws-sdk';
 import {exec} from "child_process";
-import * as async from 'async';
 
 const ecs = new ECS();
-
-function exitErr(err:any) {
-  console.log('ERROR:', err);
-  process.exit(1);
-}
 
 function exitUsage() {
   console.log('[usage]', process.argv[1], '<cluster> <family> <repository_url>');
@@ -19,97 +13,82 @@ if (process.argv.slice(2).length < 3) { exitUsage(); }
 const [cluster, family, repositoryUrl] = process.argv.slice(2);
 console.log('Deploying', family, 'to', cluster);
 
-function getVersion(cb: (error, version: string) => void) {
-  exec('git rev-parse --short HEAD', function (error, stdout) {
-    cb(error, 'v' + (stdout || '').trim());
-  });
-}
-
-function getTaskDefinitionArn(cb: (error, arn?: string) => void) {
-  ecs.listTaskDefinitions({
-    familyPrefix: family,
-    status: 'ACTIVE'
-  }, function (err, response) {
-    if (err) { return cb(err); }
-
-    if (response.taskDefinitionArns.length === 0) {
-      ecs.listTaskDefinitions({
-        familyPrefix: family,
-        status: 'INACTIVE'
-      }, function(err, response) {
-        if (err) { return cb(err); }
-        console.log(response.taskDefinitionArns);
-        cb(null, response.taskDefinitionArns[0]);
-      });
-    } else {
-      cb(null, response.taskDefinitionArns[0]);
-    }
-  });
-}
-
-function getTaskDefinition(cb: (error, definition) => void) {
-  async.waterfall([
-    getTaskDefinitionArn,
-    function (arn, cb) {
-      ecs.describeTaskDefinition({
-        taskDefinition: arn
-      }, function (err, response) {
-        cb(err, response.taskDefinition);
-      });
-    }
-  ], cb);
-}
-
-function makeTaskDefinition(
-  {taskDefinition, version}:{taskDefinition: ECS.TaskDefinition, version: string},
-  cb: (error, response?: ECS.RegisterTaskDefinitionResponse) => void
-) {
-  taskDefinition.containerDefinitions[0].image = [repositoryUrl, version].join(':');
-
-  ecs.listTaskDefinitions({
-    familyPrefix: family,
-    status: 'ACTIVE'
-  }, function(err, listResponse) {
-    if (err) { return cb(err); }
-
-    ecs.registerTaskDefinition({
-      containerDefinitions: taskDefinition.containerDefinitions,
-      family: family,
-      taskRoleArn: taskDefinition.taskRoleArn
-    }, function(err, registerResponse) {
-      if (err) { return cb(err); }
-
-      async.each(listResponse.taskDefinitionArns, function(arn, cb) {
-        ecs.deregisterTaskDefinition({
-          taskDefinition: arn
-        }, cb);
-      }, function(err) {
-        if (err) { return cb(err); }
-        cb(null, registerResponse);
-      });
+function getVersion():Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec('git rev-parse --short HEAD', function (error, stdout) {
+      if (error) { return reject(error); }
+      resolve('v' + stdout.trim());
     });
   });
 }
 
-function updateService({makeTaskDefinition}:{makeTaskDefinition: ECS.RegisterTaskDefinitionResponse}, cb: (error) => void) {
-  console.log(makeTaskDefinition);
-  const taskDef:string = [makeTaskDefinition.taskDefinition.family, makeTaskDefinition.taskDefinition.revision].join(':');
+async function getTaskDefinitionArn():Promise<string> {
+  let tasks = (await ecs.listTaskDefinitions({
+    familyPrefix: family,
+    status: 'ACTIVE'
+  }).promise()).taskDefinitionArns;
 
-  ecs.updateService({
+  if (tasks.length === 0) {
+    tasks = (await ecs.listTaskDefinitions({
+      familyPrefix: family,
+      status: 'INACTIVE'
+    }).promise()).taskDefinitionArns;
+  }
+
+  if (tasks.length === 0) {
+    throw new Error('Cannot find any tasks in ' + family);
+  }
+
+  return tasks[0];
+}
+
+async function getTaskDefinition() {
+  const arn = await getTaskDefinitionArn();
+  return (await ecs.describeTaskDefinition({
+    taskDefinition: arn
+  }).promise()).taskDefinition;
+}
+
+async function makeTaskDefinition(taskDefinition:ECS.TaskDefinition, version:string) {
+  taskDefinition.containerDefinitions[0].image = [repositoryUrl, version].join(':');
+
+  const existingTaskArns = await ecs.listTaskDefinitions({
+    familyPrefix: family,
+    status: 'ACTIVE'
+  }).promise();
+
+  const response = await ecs.registerTaskDefinition({
+      containerDefinitions: taskDefinition.containerDefinitions,
+      family: family,
+      taskRoleArn: taskDefinition.taskRoleArn
+  }).promise();
+
+  await Promise.all(existingTaskArns.taskDefinitionArns.map(arn => {
+    ecs.deregisterTaskDefinition({
+      taskDefinition: arn
+    }).promise()
+  }));
+
+  return response.taskDefinition;
+}
+
+async function updateService(taskDefinition:{family:string, revision:number}) {
+  const taskDef:string = [taskDefinition.family, taskDefinition.revision].join(':');
+
+  return ecs.updateService({
     cluster: cluster,
     service: family,
     taskDefinition: taskDef,
-  }, cb);
+  }).promise();
 }
 
-async.auto({
-  taskDefinition: getTaskDefinition,
-  version: getVersion,
-  makeTaskDefinition: ['taskDefinition', 'version', makeTaskDefinition],
-  updateService: ['makeTaskDefinition', updateService]
-}, 10, function (err, responses) {
-  if (err) {
-    exitErr(err);
-  }
-  console.log('Successfully updated task definition to image', responses.version);
-});
+async function doit() {
+  const version = await getVersion();
+  const taskDefinition = await getTaskDefinition();
+  const newDefinition = await makeTaskDefinition(taskDefinition, version);
+  await updateService(newDefinition);
+
+  console.log('Successfully updated task definition to image', version);
+}
+
+doit();

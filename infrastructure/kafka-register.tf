f56e7d2ba16c4c8c1534aa486f30067d1aa00c4b
Jeremy Wells <jemmyw@gmail.com>
2016-11-17 22:24:08 +1300

    Disable kafka-register

--- infrastructure/kafka-register.tf
+++ infrastructure/kafka-register.tf
@@ -1,10 +1,10 @@
-resource "aws_sns_topic_subscription" "kafka-register" {
-  topic_arn = "${data.terraform_remote_state.main.kafka_asg_topic_arn}"
-  protocol = "lambda"
-  endpoint = "${aws_lambda_function.kafka-register.arn}"
-
-  depends_on = ["aws_lambda_permission.kafka-register"]
-}
+# resource "aws_sns_topic_subscription" "kafka-register" {
+#   topic_arn = "${data.terraform_remote_state.main.kafka_asg_topic_arn}"
+#   protocol = "lambda"
+#   endpoint = "${aws_lambda_function.kafka-register.arn}"
+#
+#   depends_on = ["aws_lambda_permission.kafka-register"]
+# }
 
 resource "aws_lambda_permission" "kafka-register" {
   statement_id  = "AllowFromSNS"
